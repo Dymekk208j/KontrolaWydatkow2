2dfcdf37e1361998a553f5b7f3e4f823be3bc16f
Dymekk208j <Dymekk208j@gmail.com>
2017-03-24 20:40:43 +0100

    Z laptopa

--- app/src/main/java/pl/damiandziura/kontrolawydatkow/dataPicker.java
+++ app/src/main/java/pl/damiandziura/kontrolawydatkow/dataPicker.java
@@ -59,7 +59,9 @@ public class dataPicker extends AppCompatActivity {
 
         if(data.getYear() <= c.get(Calendar.YEAR) &&
            data.getMonth() <= c.get(Calendar.MONTH)+1 &&
-           data.getDayOfMonth() <= c.get(Calendar.DAY_OF_MONTH))
+           data.getDayOfMonth() <= c.get(Calendar.DAY_OF_MONTH) &&
+            czas.getCurrentHour() <= c.get(Calendar.HOUR)  &&
+            czas.getCurrentMinute() <= c.get(Calendar.MINUTE))
         {
             DATA = Integer.toString(data.getDayOfMonth()) + "-" + Integer.toString(data.getMonth()+1) + "-" + Integer.toString(data.getYear());
             String minuty, godziny;
