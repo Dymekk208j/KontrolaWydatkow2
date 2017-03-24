5133865e879e3940362dc718cfd8bbbabc45ac6c
Dymekk208j <Dymekk208j@gmail.com>
2017-03-24 16:07:13 +0100

    Test

--- app/src/main/java/pl/damiandziura/kontrolawydatkow/AddNewExpenses.java
+++ app/src/main/java/pl/damiandziura/kontrolawydatkow/AddNewExpenses.java
@@ -33,7 +33,7 @@ public class AddNewExpenses extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-//testuje git
+int testuje_gita = 2;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_expenses);
         Bundle extras = getIntent().getExtras();
