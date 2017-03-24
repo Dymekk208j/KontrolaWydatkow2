9a8883310206d4a84dc80c8390d39d6dc3f88373
Dymekk208j <Dymekk208j@gmail.com>
2017-03-24 16:21:31 +0100

    test2

--- app/src/main/java/pl/damiandziura/kontrolawydatkow/AddNewExpenses.java
+++ app/src/main/java/pl/damiandziura/kontrolawydatkow/AddNewExpenses.java
@@ -33,6 +33,7 @@ public class AddNewExpenses extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_expenses);
         Bundle extras = getIntent().getExtras();
@@ -125,7 +126,7 @@ public class AddNewExpenses extends AppCompatActivity {
                if (KWOTA > 0.0) {
                    BazaDanych.DodajWydatek(NAZWA, KWOTA, 0, 0, GODZINA, DATA);
                } else Toast.makeText(this, "Kwota musi być większa od 0!", Toast.LENGTH_SHORT).show();
-           } else Toast.makeText(this, "Musisz wpisać kwote!", Toast.LENGTH_SHORT).show();
+           } else Toast.makeText(this, "Musisz podać kwote!", Toast.LENGTH_SHORT).show();
 
         }else{Toast.makeText(this, "Musisz wpisać nazwe!", Toast.LENGTH_SHORT).show();}
 
